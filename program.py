4f4e29c9e3299e0dbed69d547044c70dd4043a8d
BatuHanHub <114951572+BatuHanHub@users.noreply.github.com>
2023-01-31 13:52:35 +0300

    Update program.py

--- program.py
+++ program.py
@@ -3,8 +3,8 @@ import os
 sayac = 0
 sayi = 1
 
-kullaniciAdi = os.environ["USERPROFILE"]
-os.chdir(f'{kullaniciAdi}\\OneDrive\\Masaüstü\\')
+kullaniciAdi = os.environ["USERPROFILE"] # Windows kullanıcı adı değişkene aktarıldı.
+os.chdir(f'{kullaniciAdi}\OneDrive\Masaüstü\\') # Program dosya yolunu masaüstüne değiştirdi  
 
 while True:
     dosya = open(f"Silemezsin {sayi}.txt", "w")
